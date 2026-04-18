62ff9b4db3141d9018efaa79d441734d5c38948e
Unbreakilo <nadasaad88h@gmail.com>
2026-04-18 18:26:22 +0200

    Delete tasks/handler.js

--- tasks/handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    await interaction.reply("🏓 Pong! Bot is working.");
-  }
-};
